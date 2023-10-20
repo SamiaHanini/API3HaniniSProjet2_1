6d7ef8290124c92b090ad2c274de6410191c6cdc
samiahanini <samiahn.pro@gmail.com>
2023-10-20 10:47:46 +0200

    First commit

---
+++ src/main/java/com/example/api3haninisprojet2_1/Api3HaniniSProjet21Application.java
@@ -0,0 +1,13 @@
+package com.example.api3haninisprojet2_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Api3HaniniSProjet21Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Api3HaniniSProjet21Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/api3haninisprojet2_1/Api3HaniniSProjet21ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.api3haninisprojet2_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Api3HaniniSProjet21ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
