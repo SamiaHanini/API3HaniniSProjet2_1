ef9b4b6f2e4f0db4dd2cfb69abe45dfbcb1b4bf7
Samia Hn <spygirl5566@gmail.com>
2024-01-10 09:35:04 +0100

    attempt debugging

--- src/main/java/be/condorcet/api3haninisprojet2_1/Api3HaniniSProjet21Application.java
+++ src/main/java/be/condorcet/api3haninisprojet2_1/Api3HaniniSProjet21Application.java
@@ -5,10 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Api3HaniniSProjet21Application {
-//modifs samedi a verifier dimanche
-    //1. mise en comm des @enablejparepositories
-    //2. redefined names for location findby client et taxi
-    //3 Checker param oui ou non
+
     public static void main(String[] args) {
         SpringApplication.run(Api3HaniniSProjet21Application.class, args);
     }
